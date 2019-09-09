356ab1a7b2f324905bd9bcf8e429f14f31a4ed11
Nicholas Wilson <njwilsondc@gmail.com>
2019-09-09 13:42:13 -0400

    Changes logic, readability

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,15 @@
 import random
+
+NUMBER_COUNT = 2
+
 print("Nicholas Wilson")
-a = random.randint(0,101)
-print(a)
-sum = a
-a = random.randint(0,101)
-print(a)
-sum += a
+
+sum = 0
+for x in range(NUMBER_COUNT):
+    x = random.randint(0,101)
+    sum += x
+    print(x)
+
+
 print("Sum = ", sum)
-print("Average = ", sum/2)
+print("Average = ", sum/NUMBER_COUNT)
